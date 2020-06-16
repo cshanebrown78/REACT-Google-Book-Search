4f5c825b48ed8c4265321b8f802ba43340c4ec13
C Shane Brown <cshanebrown78@gmail.com>
2020-06-16 12:05:48 -0500

    finished Heroku deploy coding

---
+++ client/build/precache-manifest.99e85f8046ee32e4f0e6eb37306638ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "048126a5c914c0467499",
+    "url": "/static/css/main.98833908.chunk.css"
+  },
+  {
+    "revision": "048126a5c914c0467499",
+    "url": "/static/js/main.c5b5447b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "043dcaa302a6f6ae3c8a",
+    "url": "/static/js/2.a313f46f.chunk.js"
+  },
+  {
+    "revision": "e2b3b1c61df96348137f3d49d9f194fc",
+    "url": "/index.html"
+  }
+];
